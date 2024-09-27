8be7df3ae96320d2a5adddc9b25dc78587b2d661
User <User@BAD-BOY99>
2024-09-27 13:51:01 +0200

    total sum

---
+++ Homework L2.2/src/apples/Total.java
@@ -0,0 +1,12 @@
+package apples;
+
+public class Total {
+
+	public static void main(String[] args) {
+		int appleCost = 3;
+		int quantity = 6;
+		int totalSum ;
+		totalSum = appleCost * quantity ;
+		System.out.println(totalSum + " $");		
+	}
+}
