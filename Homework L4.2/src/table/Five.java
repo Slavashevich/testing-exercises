7a011d53ea73651c5f257fa12768efc75db7b7c0
User <User@BAD-BOY99>
2024-10-03 21:04:54 +0200

    5 times table

---
+++ Homework L4.2/src/table/Five.java
@@ -0,0 +1,12 @@
+package table;
+public class Five {
+
+	public static void main(String[] args) {
+		int five = 5;
+		int sum;
+		for (int i=1; i<=10; i++) {
+			sum = five * i;
+		System.out.println("5*"+ i + "="+ sum);	
+		}
+	}
+}
